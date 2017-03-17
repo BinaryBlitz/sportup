0f7828993671d991003c6d43beeae8f034f2b126
SumLare <mrhard95@gmail.com>
2017-03-18 00:33:30 +0300

    Create sport type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170317204916) do
+ActiveRecord::Schema.define(version: 20170317212133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20170317204916) do
     t.string   "name",       null: false
     t.float    "latitude",   null: false
     t.float    "longitude",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sport_types", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "color",      null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
