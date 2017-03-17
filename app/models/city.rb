dee27cd6def894dff721f3b7951fe0d3fbea2b16
SumLare <mrhard95@gmail.com>
2017-03-18 00:07:18 +0300

    Create city

---
+++ app/models/city.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: cities
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  latitude   :float            not null
+#  longitude  :float            not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class City < ApplicationRecord
+  validates :name, presence: true
+  validates :latitude, inclusion: { in: -90..90 }
+  validates :longitude, inclusion: { in: -180..180 }
+end
