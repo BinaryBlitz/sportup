ca3340f6c66c82c0026dd3176a1cbcfc7f127ac9
SumLare <mrhard95@gmail.com>
2017-03-18 18:12:45 +0300

    Events (#6)
    * Create event
    
    * Add events to user
    
    * API: events
    
    * Events tests

---
+++ test/controllers/api/events_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class API::EventsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @event = events(:event)
+  end
+
+  test 'should get show' do
+    get api_event_path(@event, api_token: api_token)
+    assert_response :success
+  end
+
+  test 'should create event' do
+    @event.destroy
+
+    assert_difference 'Event.count' do
+      post api_events_url(api_token: api_token), params: { event: @event.attributes }
+    end
+
+    assert_response :created
+  end
+
+  test 'should update event' do
+    new_name = 'Another name'
+    patch api_event_path(@event, api_token: api_token), params: { event: { name: new_name } }
+    assert_response :ok
+    assert_equal new_name, @event.reload.name
+  end
+end
