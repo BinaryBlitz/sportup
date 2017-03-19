2b29490bd859dcd02844ebc0ce95e9839b01c385
SumLare <mrhard95@gmail.com>
2017-03-19 15:35:11 +0300

    Events tests

---
+++ test/models/membership_test.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: memberships
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  event_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'test_helper'
+
+class MembershipTest < ActiveSupport::TestCase
+  setup do
+    @membership = memberships(:membership)
+  end
+
+  test 'valid' do
+    assert @membership.valid?
+  end
+end
