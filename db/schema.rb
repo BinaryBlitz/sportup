2ed7ff3b5e603c61cfc0e1782e30fff001fc200b
SumLare <mrhard95@gmail.com>
2017-03-20 22:47:27 +0300

    Create team

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320004427) do
+ActiveRecord::Schema.define(version: 20170320194459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,16 @@ ActiveRecord::Schema.define(version: 20170320004427) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "icon",       null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.integer  "number",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_teams_on_event_id", using: :btree
+    t.index ["user_id"], name: "index_teams_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
