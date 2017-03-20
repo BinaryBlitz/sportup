59606da0493c73fcb4430b0c49e263230d56a3bd
SumLare <mrhard95@gmail.com>
2017-03-20 03:23:11 +0300

    API: user memberships

--- app/controllers/api/memberships_controller.rb
+++ app/controllers/api/memberships_controller.rb
@@ -1,6 +1,10 @@
 class API::MembershipsController < API::APIController
   before_action :set_membership, only: [:destroy]
 
+  def index
+    @memberships = current_user.memberships
+  end
+
   def destroy
     @membership.destroy
     head :no_content
