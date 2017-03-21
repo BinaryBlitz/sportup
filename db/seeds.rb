0cfcc99eaa579ca7939257dddc143ced796e89c8
SumLare <mrhard95@gmail.com>
2017-03-21 20:20:52 +0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
 city = City.create!(name: 'City', latitude: 0.0, longitude: 0.0)
 
+image = File.open(Rails.root.join('test', 'fixtures', 'files', 'blank.jpg'))
+
+sport_type = SportType.create!(name: 'Football', color: '#fff', icon: image)
+
 user = User.create!(
   first_name: 'Foo', last_name: 'Bar', phone_number: '+79998887766'
 )
 
 user.update!(api_token: 'foobar')
+
+event = Event.create!(
+  name: 'Event', description: 'Description', address: 'Address',
+  longitude: 0.0, latitude: 0.0, starts_at: 1.month.from_now, ends_at: '23:59',
+  user_limit: 2, team_limit: 2, price: 1, sport_type: sport_type, creator: user
+)
+
