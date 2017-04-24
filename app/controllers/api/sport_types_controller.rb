5769ae21ad453305f98feec3543028539ff47eb2
SumLare <mrhard95@gmail.com>
2017-04-25 02:51:02 +0300

    API: remove city from sport types index

--- app/controllers/api/sport_types_controller.rb
+++ app/controllers/api/sport_types_controller.rb
@@ -1,14 +1,7 @@
 class API::SportTypesController < API::APIController
   skip_before_action :restrict_access!, only: [:index]
-  before_action :set_city, only: [:index]
 
   def index
-    @sport_types = SportType.by_city(@city)
-  end
-
-  private
-
-  def set_city
-    @city = City.find(params[:city_id])
+    @sport_types = SportType.all
   end
 end
