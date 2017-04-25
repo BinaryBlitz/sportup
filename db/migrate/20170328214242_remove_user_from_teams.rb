44cfcc3b56659b6b69c5d94bcc7bda276f194a7f
SumLare <mrhard95@gmail.com>
2017-04-25 03:11:18 +0300

    Remove teams related migrations

--- db/migrate/20170328214242_remove_user_from_teams.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserFromTeams < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :teams, :user
-    add_foreign_key :teams, :events
-  end
-end
--- db/migrate/20170330210414_drop_teams_table.rb
+++
@@ -1,6 +0,0 @@
-class DropTeamsTable < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :teams, force: :cascade
-    drop_table :joins, force: :cascade
-  end
-end
