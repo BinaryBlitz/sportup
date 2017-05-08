d7660af4e9043e04bd18f6546bbdbed49ac4ea57
SumLare <mrhard95@gmail.com>
2017-05-08 18:45:22 +0300

    Use users table name in telegram bot user

--- app/models/telegram_bot_user.rb
+++ app/models/telegram_bot_user.rb
@@ -1,8 +1,8 @@
 class TelegramBotUser < TelegramBot
-  table_name = 'users'
+  self.table_name = 'users'
 
   def self.find_or_create(user)
-    find_or_create_by_telegram_id(user.telegram_id) do |bot_user|
+    find_or_create_by(telegram_id: user.telegram_id) do |bot_user|
       bot_user.first_name = user.first_name
       bot_user.last_name = user.last_name
     end
