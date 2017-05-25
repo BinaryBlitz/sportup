dd31f1c9ff32b8da1be38977621396cfa86d3cac
SumLare <mrhard95@gmail.com>
2017-05-25 17:43:34 +0300

    Fix models code style

--- app/models/telegram_bot_event.rb
+++ app/models/telegram_bot_event.rb
@@ -3,10 +3,11 @@ class TelegramBotEvent < TelegramBot
 
   default_scope { where(closed: false) }
 
+  belongs_to :chat, class_name: 'TelegramBotChat', foreign_key: 'chat_id'
+
   has_many :users, through: :memberships
   has_many :memberships, class_name: 'TelegramBotMembership', foreign_key: 'event_id'
   has_many :guests, class_name: 'TelegramBotGuest', foreign_key: 'event_id', dependent: :destroy
-  belongs_to :chat, class_name: 'TelegramBotChat', foreign_key: 'chat_id'
 
   def members_count
     users.count + guests.count
